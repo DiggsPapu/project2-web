d1c902d2f0ef3c25d3380b9207d0c9e251d00a69
dieggspapu <alo20172@uvg.edu.gt>
2023-05-22 11:19:04 -0600

    Fetch getting 25 players

--- services/fifaservice.service.ts
+++ services/fifaservice.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from 'src/environments/environment';
+
+export const PLAYER_TABLE = 'player'
+export const TEAM_TABLE = 'team'
+export const ALL = '*'
 
 @Injectable({
   providedIn: 'root'
 })
 export class FifaserviceService {
-
-  constructor() { }
+  private supabase: SupabaseClient;
+  constructor() {
+    this.supabase = createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey
+    )
+   }
+  async getAllPlayers() {
+    let { data: player, error } = await this.supabase.from(PLAYER_TABLE).select(ALL).limit(25)
+    return player || []
+  }
+  async getPlayer(player_id: number) {
+    const player = await this.supabase.from(PLAYER_TABLE).select(ALL).eq('player_id', player_id)
+    return player || null
+  }
+  async getTeam(team_id: number) {
+    const team = await this.supabase.from(TEAM_TABLE).select(ALL).eq('team_id', team_id)
+    return team || null
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FifaserviceService } from 'services/fifaservice.service';
 
 @Component({
   selector: 'app-root', //This is the thing that calls the component
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'web-project2';
+export class AppComponent  implements OnInit{
+  lists: any[] = []
+  title = 'To-Do List'
+  listCards: any = {}
+  editCard: any = {}
+  constructor(
+    private route: ActivatedRoute,
+    private fifaService: FifaserviceService,
+    private router: Router) { }
+  async ngOnInit() {
+    this.listCards = await (this.fifaService.getAllPlayers())
+    console.log(this.listCards)
+  }
 }
