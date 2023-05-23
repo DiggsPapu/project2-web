1a530361c6a7600b4c2e3effc755e3670553d024
dieggspapu <alo20172@uvg.edu.gt>
2023-05-22 22:21:01 -0600

    Image card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
